baedc86667c3fe7095808bf216d17b134c643da2
Mandrenkov <M.Andrenkov@gmail.com>
2019-05-05 13:15:20 -0400

    Added asynchronous batch polling to Manager and used Album instances to manage Video collections.

--- js/manager.js
+++ js/manager.js
@@ -1,74 +1,78 @@
-// The "manager.js" script implements the Manager class.
+// This script implements the Manager class.
 // -----------------------------------------------------------------------------
 
 // Manager manages a collection of Videos.
 class Manager {
     // Constructs a new Manager object.
     constructor() {
-        this.videos = [];
-        this.delay = undefined;
-        this.timeout = undefined;
+        this.album = new Album();
 
+        // Determines whether this Manager is ready to poll the DOM.
+        this.ready = true;
+
+        this.restore();
+        this.listen();
+    }
+
+    // Restores the Manager state using the synchronized Chrome storage API.
+    restore() {
         // Retrieve the "Hide Videos" checkbox state using the synchronized Chrome storage API.
         chrome.storage.sync.get("hide", items => {
-            let valid = !chrome.runtime.lastError && items.hasOwnProperty("hide");
+            const valid = !chrome.runtime.lastError && items.hasOwnProperty("hide");
             this.hidden = valid ? items["hide"] : Manager.DEFAULT_HIDDEN;
-            console.post("Manager.ctor(): Setting hidden to", this.hidden, ".");
+            Logger.info("Manager.restore(): Setting hidden to", this.hidden, ".");
         });
 
         // Retrieve the "View Threshold" slider state using the synchronized Chrome storage API.
         chrome.storage.sync.get("threshold", items => {
-            let valid = !chrome.runtime.lastError && items.hasOwnProperty("threshold");
+            const valid = !chrome.runtime.lastError && items.hasOwnProperty("threshold");
             this.threshold = valid ? items["threshold"] : Manager.DEFAULT_THRESHOLD;
-            console.post("Manager.ctor(): Setting threshold to", this.threshold, ".");
+            Logger.info("Manager.restore(): Setting threshold to", this.threshold, ".");
         });
-
-        this.start();
     }
 
-    // Starts the Manager refresh loop.
-    start() {
-        console.post("Manager.start(): Starting the Manager refresh loop.");
-        this.delay = Manager.DEFAULT_DELAY;
-        this.timeout = setTimeout(() => this.loop(), 0);
+    // Attaches a MutationObserver which listens for DOM mutation events to this Manager.
+    listen() {
+        // Construct a MutationObserver with a callback that batches invocations to poll().
+        const observer = new MutationObserver(() => this.request());
+
+        // Attach the MutationObserver to all childList mutations in the DOM.
+        const node = document.getRootNode();
+        const conf = {childList: true, subtree: true};
+        observer.observe(node, conf);
     }
 
-    // Continuously updates the Video collection of this Manager by polling the
-    // DOM for new Videos.  The timeout delay of the loop timeout is doubled
-    // every iteration of the loop.
-    loop() {
-        let updated = this.poll();
-        this.delay = updated ? Manager.DEFAULT_DELAY : 2*this.delay;
-        this.timeout = setTimeout(() => this.loop(), this.delay);
-    }
+    // -------------------------------------------------------------------------
 
-    // Stops the Manager refresh loop and clears any pending refresh timeouts.
-    stop() {
-        console.post("Manager.stop(): Stopping the Manager refresh loop.");
-        if (this.timeout) {
-            clearTimeout(this.timeout);
+    // Requests this Manager to poll the DOM.  Poll requests that are issued in
+    // rapid succession are batched into a single poll request.
+    request() {
+        if (this.ready) {
+            this.ready = false;
+            setTimeout(() => this.poll(), Manager.BATCH_TIME);
         }
-        this.timeout = undefined;
     }
 
-    // Polls the DOM for new Videos and updates the Video collection if necessary. 
+    // Polls the DOM for new Videos and updates the Video collection if necessary.
     poll() {
-        let videos = this.fetchVideos();
-        let updated = !this.sameVideos(this.videos, videos);
-        if (updated) {
-            this.updateVideos(videos);
-            console.post("Manager.poll(): Updated Videos to", this.videos, ".");
-        } else {
-            console.post("Manager.poll(): No Video changes were detected.");
-        }
-        this.refreshVideos();
-        return updated;
+        // Construct an Album from each of the viewed Videos in the DOM.
+        const videos = this.fetchVideos();
+        const album = new Album(videos);
+        videos.forEach(video => album.add(video));
+
+        // Incorporate the new Album and refresh the visibilities of the Videos.
+        this.album.merge(album);
+        this.refresh();
+
+        // Reset the batch MutationObserver flag.
+        this.ready = true;
     }
 
-    // Returns an array containing all the Videos in the current DOM.
+    // -------------------------------------------------------------------------
+
+    // Fetches all the viewed Videos in the current DOM.
     fetchVideos() {
-        let elements = [].concat(
-            this.fetchAutoplayVideos(),
+        const elements = [].concat(
             this.fetchSectionVideos(document),
             this.fetchGridVideos(document),
             this.fetchSearchVideos(document),
@@ -76,88 +80,58 @@ class Manager {
             this.fetchShelfVideos(document)
         );
 
-        let videos = this.uniquify(elements.map(element => new Video(element, this)));
-        let viewed = videos.filter(video => video.getViewed());
-
-        console.post("Manager.fetchVideos(): %d/%d Videos on the page were viewed.", viewed.length, videos.length);
+        // Construct a list of viewed Videos from the Video elements.
+        const videos = elements.map(element => new Video(element, this));
+        const viewed = videos.filter(video => video.getViewed());
+        Logger.info("Manager.fetchVideos(): %d/%d Videos on the page were viewed.", viewed.length, videos.length);
         return viewed;
     }
 
-    // Returns an array containing all the section Videos in the given HTML element.
+    // Fetches all the section Videos in the given HTML element.
     fetchSectionVideos(element) {
-        let elements = Array.from(element.querySelectorAll("ytd-item-section-renderer.style-scope.ytd-section-list-renderer"));
+        const elements = Array.from(element.querySelectorAll("ytd-item-section-renderer.style-scope.ytd-section-list-renderer"));
         return elements.filter(element => this.fetchGridVideos(element).length      == 1
                                        || this.fetchSearchVideos(element).length    == 1
                                        || this.fetchSecondaryVideos(element).length == 1 
                                        || this.fetchShelfVideos(element).length     == 1);
     }
 
-    // Returns an array containing all the grid Videos in the given HTML element.
+    // Fetches all the grid Videos in the given HTML element.
     fetchGridVideos(element) {
         return Array.from(element.querySelectorAll(":scope ytd-grid-video-renderer.style-scope.yt-horizontal-list-renderer")).concat(
                Array.from(element.querySelectorAll(":scope ytd-grid-video-renderer.style-scope.ytd-grid-renderer")));
     }
 
-    // Returns an array containing all the search Videos in the given HTML element.
+    // Fetches all the search Videos in the given HTML element.
     fetchSearchVideos(element) {
         return Array.from(element.querySelectorAll(":scope ytd-video-renderer.style-scope.ytd-item-section-renderer")).concat(
                Array.from(element.querySelectorAll(":scope ytd-video-renderer.style-scope.ytd-vertical-list-renderer")));
     }
 
-    // Returns an array containing all the secondary Videos in the given HTML element.
+    // Fetches all the secondary Videos in the given HTML element.
     fetchSecondaryVideos(element) {
         return Array.from(element.querySelectorAll(":scope ytd-compact-video-renderer.style-scope.ytd-watch-next-secondary-results-renderer"));
     }
 
-    // Returns an array containing all the shelf Videos in the given HTML element.
+    // Fetches all the shelf Videos in the given HTML element.
     fetchShelfVideos(element) {
         return Array.from(element.querySelectorAll(":scope ytd-video-renderer.style-scope.ytd-expanded-shelf-contents-renderer"));
     }
 
-    // Returns true if the given arrays of Videos represent the same Videos.
-    sameVideos(videosA, videosB) {
-        if (videosA === undefined || videosB === undefined) {
-            return false;
-        }
+    // -----------------------------------------------------------------------------
 
-        let setA = new Set(videosA.map(video => video.getID()));
-        let setB = new Set(videosB.map(video => video.getID()));
-
-        // Two Sets are the same if:
-        //    1. The Sets contain the same number of elements.
-        //    2. Every element in the first Set appears in the second Set.
-        let same = true;
-        same = same && setA.size == setB.size;
-        same = same && Array.from(setA).every(id => setB.has(id));
-        return same;
-    }
-
-    // Returns a unique array of the given Videos.
-    uniquify(videos) {
-        let ids = videos.map(video => video.getID());
-        return videos.filter((video, index) => ids.indexOf(video.getID()) == index);
-    }
-
-    // Updates the collection of Videos belonging to this Manager.
-    // Any Videos that are dropped from the collection are reset to their visible state.
-    updateVideos(videos) {
-        let newIDs = new Set(videos.map(video => video.getID()));
-        let droppedVideos = this.videos.filter(video => !newIDs.has(video.getID()));
-        droppedVideos.forEach(video => video.show());
-
-        this.videos = videos;
-    }
-
-    // Refreshes the visible state of all Videos belonging to this Manager.
-    refreshVideos() {
-        console.post("Manager.refreshVideos():", this.hidden ? "Hiding" : "Showing", "Videos.");
-        this.videos.forEach(video => this.hidden ? video.hide() : video.show());
+    // Refreshes the Album associated with this Manager.
+    refresh() {
+        Logger.info("Manager.refresh():", this.hidden ? "Hiding" : "Showing", "Videos.");
+        this.album.refresh(!this.hidden);
     }
 }
 
 // -----------------------------------------------------------------------------
 
-// Default Manager values.
-Manager.DEFAULT_DELAY = 100;
+// Default "Hide Videos" and "View Threshold" state values.
 Manager.DEFAULT_HIDDEN = false;
 Manager.DEFAULT_THRESHOLD = 90;
+
+// Time interval to batch poll requests.
+Manager.BATCH_TIME = 200;
