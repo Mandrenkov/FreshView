e3525afb4dafe21b26841628a77f598f2b0f80de
Mandrenkov <M.Andrenkov@gmail.com>
2022-01-09 15:09:14 -0500

    Add constants for storage keys

--- js/constants.js
+++ js/constants.js
@@ -1,27 +1,38 @@
-// Default Dark Mode checkbox state.
-const DEFAULT_DARK_MODE_CHECKBOX_STATE = false;
+// This file contains constants that may be shared across different scripts.
+// -----------------------------------------------------------------------------
 
-// Default Filters (Types) checkbox states.
-const DEFAULT_HIDE_RECOMMENDATIONS_CHECKBOX_STATE = true;
-const DEFAULT_HIDE_PLAYLISTS_CHECKBOX_STATE = true;
-const DEFAULT_HIDE_SEARCHES_CHECKBOX_STATE = true;
-const DEFAULT_HIDE_CHANNELS_CHECKBOX_STATE = true;
+// Dark Mode
+const DARK_MODE_CHECKBOX_STORAGE_KEY = "dark-mode-checkbox-state";
+const DARK_MODE_CHECKBOX_DEFAULT_STATE = false;
 
-// Default Filters (Pages) checkbox states.
+// Filters (Types)
+const HIDE_RECOMMENDATIONS_CHECKBOX_STORAGE_KEY = "hide-recommendations-checkbox-state";
+const HIDE_RECOMMENDATIONS_CHECKBOX_DEFAULT_STATE = true;
+const HIDE_PLAYLISTS_CHECKBOX_STORAGE_KEY = "hide-playlists-checkbox-state";
+const HIDE_PLAYLISTS_CHECKBOX_DEFAULT_STATE = true;
+const HIDE_SEARCHES_CHECKBOX_STORAGE_KEY = "hide-searches-checkbox-state";
+const HIDE_SEARCHES_CHECKBOX_DEFAULT_STATE = true;
+const HIDE_CHANNELS_CHECKBOX_STORAGE_KEY = "hide-channels-checkbox-state";
+const HIDE_CHANNELS_CHECKBOX_DEFAULT_STATE = true;
+
+// Filters (Pages)
 const DEFAULT_HIDE_HOME_CHECKBOX_STATE = true;
 const DEFAULT_HIDE_EXPLORE_CHECKBOX_STATE = true;
 const DEFAULT_HIDE_SUBSCRIPTIONS_CHECKBOX_STATE = true;
 const DEFAULT_HIDE_LIBRARY_CHECKBOX_STATE = true;
 const DEFAULT_HIDE_HISTORY_CHECKBOX_STATE = false;
 
-// Default View Threshold checkbox state and slider value.
-const DEFAULT_VIEW_THRESHOLD_CHECKBOX_STATE = true;
-const DEFAULT_VIEW_THRESHOLD_SLIDER_VALUE = 90;
+// View Threshold
+const VIEW_THRESHOLD_CHECKBOX_STORAGE_KEY = "view-threshold-checkbox-state";
+const VIEW_THRESHOLD_CHECKBOX_DEFAULT_STATE = true;
+const VIEW_THRESHOLD_SLIDER_STORAGE_KEY = "view-threshold-slider-value";
+const VIEW_THRESHOLD_SLIDER_DEFAULT_STATE = 90;
 
-// Default Hide Videos checkbox state and bookmarks.
-const DEFAULT_HIDE_VIDEOS_CHECKBOX_STATE = false;
-const DEFAULT_HIDE_VIDEOS_BOOKMARK_STATE = false;
-const DEFAULT_HIDE_VIDEOS_BOOKMARKS = {
+// Hide Videos
+const HIDE_VIDEOS_CHECKBOX_STORAGE_KEY = "hide-videos-checkbox-state";
+const HIDE_VIDEOS_CHECKBOX_DEFAULT_STATE = false;
+const HIDE_VIDEOS_BOOKMARKS_STORAGE_KEY = "hide-videos-bookmarks";
+const HIDE_VIDEOS_BOOKMARKS_DEFAULT_STATE = {
     "/": DEFAULT_HIDE_HOME_CHECKBOX_STATE,
     "/feed/explore": DEFAULT_HIDE_EXPLORE_CHECKBOX_STATE,
     "/feed/subscriptions": DEFAULT_HIDE_SUBSCRIPTIONS_CHECKBOX_STATE,
@@ -29,5 +40,21 @@ const DEFAULT_HIDE_VIDEOS_BOOKMARKS = {
     "/feed/history": DEFAULT_HIDE_HISTORY_CHECKBOX_STATE,
 };
 
-// Number of milliseconds over which poll requests should be batched together.
-const POLL_BATCH_TIME_MILLISECONDS = 200;
+// -----------------------------------------------------------------------------
+
+// Number of milliseconds over which poll requests should be batched together
+const BATCH_TIME_MILLISECONDS = 50;
+
+// -----------------------------------------------------------------------------
+
+// Default Settings state
+const SETTINGS_DEFAULT_STATE = {
+    [HIDE_VIDEOS_CHECKBOX_STORAGE_KEY]: HIDE_VIDEOS_CHECKBOX_DEFAULT_STATE,
+    [HIDE_VIDEOS_BOOKMARKS_STORAGE_KEY]: HIDE_VIDEOS_BOOKMARKS_DEFAULT_STATE,
+    [VIEW_THRESHOLD_CHECKBOX_STORAGE_KEY]: VIEW_THRESHOLD_CHECKBOX_DEFAULT_STATE,
+    [VIEW_THRESHOLD_SLIDER_STORAGE_KEY]: VIEW_THRESHOLD_SLIDER_DEFAULT_STATE,
+    [HIDE_RECOMMENDATIONS_CHECKBOX_STORAGE_KEY]: HIDE_RECOMMENDATIONS_CHECKBOX_DEFAULT_STATE,
+    [HIDE_PLAYLISTS_CHECKBOX_STORAGE_KEY]: HIDE_PLAYLISTS_CHECKBOX_DEFAULT_STATE,
+    [HIDE_SEARCHES_CHECKBOX_STORAGE_KEY]: HIDE_SEARCHES_CHECKBOX_DEFAULT_STATE,
+    [HIDE_CHANNELS_CHECKBOX_STORAGE_KEY]: HIDE_CHANNELS_CHECKBOX_DEFAULT_STATE
+}
